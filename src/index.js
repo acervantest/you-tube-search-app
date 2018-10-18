fc2da45558f51a8b2ecec57064595743d6bb3ad2
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-10-18 15:23:16 -0700

    Class based search_bar component implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+import SearchBar from './components/search_bar';
 
 const API_KEY = [ API_KEY ];
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Youtube Search APP [React, Redux, Webpack]</h1>
+      <div>
+        <SearchBar />
+      </div>
     )
   }
 }
