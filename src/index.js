86d12497ea900f1591c2f9a6bc859544cfe0d0b1
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-10-22 13:27:27 -0700

    VideoList component implementation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = [ API_KEY ];
 
-class App extends React.Component {
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: []
+    }
+
+    YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={ this.state.videos }/>
       </div>
     )
   }
