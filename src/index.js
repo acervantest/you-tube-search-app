95926469dcca3dbf4aff82e4de1dd02c9bd0dfd4
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-10-23 16:39:49 -0700

    Debounce using lodash implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
@@ -31,9 +32,12 @@ class App extends Component {
   }
 
   render() {
+
+    const videoSearch = _.debounce( (term)=>{ this.videoSearch(term) }, 300 );
+
     return (
       <div>
-        <SearchBar videoSearchChange={ term => this.videoSearch(term) }/>
+        <SearchBar videoSearchChange={ videoSearch }/>
         <VideoDetail video={ this.state.selectedVideo  }/>
         <VideoList
           videos={ this.state.videos }
